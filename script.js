bd14d9a06cdcac390c22eeabbf3162dabda3e188
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2024-05-21 12:10:24 +0200

    Global selectors added and code to change button on read status

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const button = document.querySelectorAll('.remove');
 const books = document.querySelector('.books')
 const cards = document.querySelectorAll('.card')
+const addBookButton = document.querySelector('.addbook-section button')
+const readButton = document.querySelectorAll('.read')
+const tickSymbol = document.querySelectorAll('#tick')
+const myLibrary =  [];
+console.log(tickSymbol);
+function Book () {
+
+}
 
 
+function addBookToLibrary () {
+
+}
 
 
 
@@ -14,5 +25,30 @@ books.addEventListener('click', (e) => {
         }
     })
 
+
+//code to change tickSymbol to X when unread
+
+ readButton.forEach((button) => {
+        button.addEventListener("click", () => {
+                if (button.textContent === 'Read') {
+                        button.textContent = 'Not Read';
+                        button.style.background = 'red';
+                                for(const symbol of tickSymbol) {
+                                        symbol.textContent = 'X';
+                                        symbol.style.color = 'red';
+                                }
+                }
+                else if (button.textContent === 'Not Read') {
+                        button.textContent = 'Read';
+                        button.style.background = 'Green'
+                                for(const symbol of tickSymbol) {
+                                        symbol.textContent = '✓';
+                                        symbol.style.color = 'green'
+                                }
+                }
+                })}
+  
+ );
+        
 
 
